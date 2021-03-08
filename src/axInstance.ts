65ebab1f1d2369e6a2a30e2ec9e7994008edef6d
Tosin Kuye <tkuye77@gmail.com>
2021-03-08 16:53:28 -0700

    changed ax Instance build

--- src/axInstance.ts
+++ src/axInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL = 'http://localhost:8000/api/v1'
+const baseURL = process.env.NODE_ENV === 'production'? "/api/v1":'http://localhost:8000/api/v1'
 
 export default axios.create({
     baseURL,
